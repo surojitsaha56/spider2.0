1fdd7415b5fd97a0b96ebe4c16f8c3790b79efcd
Surojit Saha <sahasurojit06@gmail.com>
2021-09-04 23:15:28 +0530

    WeatherBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './components/WeatherBar'
+import WeatherBar from './components/WeatherBar';
 
 function App() {
   return (
     <div className="App">
       <h1>Spider 2.0</h1>
+      <WeatherBar/>
     </div>
   );
 }
---
+++ src/components/WeatherBar.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+import mumbai from '../images/mumbai.jpg'
+import london from '../images/london.jpg'
+import new_york from '../images/new_york.jpg'
+import './WeatherBar.css';
+
+class WeatherBar extends React.Component {
+
+    state={
+        days:[
+            {'Mumbai':{temperature: '32&deg;C'},'London':{temperature: '22&deg;C'},'New York':{temperature: '12&deg;C'}},
+            {'Mumbai':{temperature: '31&deg;C'},'London':{temperature: '21&deg;C'},'New York':{temperature: '11&deg;C'}},
+            {'Mumbai':{temperature: '30&deg;C'},'London':{temperature: '20&deg;C'},'New York':{temperature: '10&deg;C'}},
+            {'Mumbai':{temperature: '32&deg;C'},'London':{temperature: '22&deg;C'},'New York':{temperature: '12&deg;C'}},
+            {'Mumbai':{temperature: '31&deg;C'},'London':{temperature: '21&deg;C'},'New York':{temperature: '11&deg;C'}},
+            
+        ]
+    }
+    display = (id) => {
+        document.getElementById("M-date").innerHTML = this.state.days[id].Mumbai.temperature;
+        document.getElementById("L-date").innerHTML = this.state.days[id].London.temperature;
+        document.getElementById("NY-date").innerHTML = this.state.days[id]['New York'].temperature;
+    }
+
+    render() { 
+        return(
+            <div>
+                <div className="container">
+                    <button className="waves-effect waves-light btn secondary btn-large " onClick={() => this.display(0)}>Monday</button>
+                    <button className="waves-effect waves-light btn secondary btn-large" onClick={() => this.display(1)}>Tuesday</button>
+                    <button className="waves-effect waves-light btn secondary btn-large" onClick={() => this.display(2)}>Wednesday</button>
+                    <button className="waves-effect waves-light btn secondary btn-large" onClick={() => this.display(3)}>Thursday</button>
+                    <button className="waves-effect waves-light btn secondary btn-large" onClick={() => this.display(4)}>Friday</button>
+                </div>
+                <br />
+                <div className="row container">
+                    <div className="col s4 card">
+                        <br />
+                        <div className="card-image">
+                            <img src={mumbai} alt="" />
+                        </div>
+                        <div className="card-title">
+                            <p>Mumbai</p>
+                        </div>
+                        
+                        <p id="M-date">32&deg;C</p>
+                    </div>
+                    <div className="col s4 card">
+                        <br/>
+                        <div className="card-image">
+                            <img src={london} alt="" />
+                        </div>
+                        <div className="card-title">
+                            <p>London</p>
+                        </div>
+                        
+                        <p id="L-date">22&deg;C</p>
+                    </div>
+                    <div className="col s4 card">
+                        <br />
+                        <div className="card-image">
+                            <img src={new_york} alt="" />
+                        </div>
+                        <div className="card-title">
+                            <p>New York</p>
+                        </div>
+                        
+                        <p id="NY-date">12&deg;C</p>
+                    </div>
+                </div>
+            </div>
+            
+        ) 
+    }
+}
+ 
+export default WeatherBar;
