0ecf9b78e63c70598f2569feaca8a776b39fedd3
Chintan <chintanshah2587@gmail.com>
2021-09-05 11:53:22 +0530

    Nav-Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import NavbarComp from './components/NavbarComp';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Spider 2.0</h1>
-    </div>
-  );
+ <div className="App">
+   <NavbarComp/>
+ </div>
+    );
 }
 
 export default App;
